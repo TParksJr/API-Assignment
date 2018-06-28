2b0b7e9137e4f32a4db4e3a6a7950998fa9c0c59
Tyson <tys_p@yahoo.ca>
2018-06-27 22:45:23 -0400

    splitting css and js into separate files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,34 @@
-$(document).ready(function () {
+$(function() {
+    //declaring variables
+    var giphyAPIKey = "DPJ6mgUrj8JV2BoktzbF6voOqEpNjNys",
+        queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + giphyAPIKey + "&limit=10",
+        searchTerm = "",
+        input = "",
+        animals = ["cat", "dog", "bear", "horse", "skunk", "raccoon", "bird", "deer", "moose", "rabbit", "hamster", "turtle", "chicken", "pig", "goat", "sheep", "hedgehog", "gineapig"];
     
+    //generating search buttons
+    for (i = 0; i < animals.length; i++) {
+        $("#buttons").append("<button class='animalButton'>" + animals[i] + "</button>").data("animal", animals[i]);
+    };
+
+    //function to add new buttons *** does not work - the added button immediately disapears ***
+    $("#submit").on("click", function() {
+        input = $("#input").val().trim();
+        input.push(animals);
+        $("#buttons").append("<button class='animalButton'>" + $("#input").val().trim() + "</button>");
+    });
+
+    //function to get data from Giphy
+    $(".animalButton").on("click", function() {
+        searchTerm = "cat";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            $("#gifs").empty();
+            for (i = 0; i < response.data.length; i++) {
+                $("#gifs").append("<img src='https://media.giphy.com/media/" + response.data[i].id + "/giphy.gif'>");
+            };
+        }); 
+    });
 });
